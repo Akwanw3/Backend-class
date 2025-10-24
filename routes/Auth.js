7829d940f8081090b55177fcb59fe77ad8c861e6
ABIA AKPE <akwanwesunday@gmail.com>
2025-10-24 17:39:53 +0100

    initial commit

---
+++ routes/Auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    login, 
+    register,
+} = require("../controller/Auth");
+
+const { validateLoginObj, validateRegisterObj } = require('../validators/Auth/index');
+
+
+router.post('/login', validateLoginObj,  login);
+router.post('/register', validateRegisterObj, register);
+
+module.exports = router;
